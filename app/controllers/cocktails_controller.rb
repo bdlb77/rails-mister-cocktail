742e1123c9623f163f8c0322d91103451d753b92
Bryan Leighton <bryan.leigh77@gmail.com>
2018-05-30 13:33:08 +0300

    cocktail and dose routes/controllers

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,29 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show]
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def create
+   Cocktail.create(cocktail_params)
+   redirect_to cocktails_path
+  end
+  
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def show
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+puts "commencing seed."
+request_uri = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+url = "#{request_uri}"
+buffer_url = open(url).read
+
+ingredients = JSON.parse(buffer_url)
+
+ingredients["drinks"].each do |ingredient|
+	ingredient["strIngredient1"]
+end
+puts "finished!"
