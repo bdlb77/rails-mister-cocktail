60ed035bba9355e2909b6704997d0552dfbf5d1b
Bryan Leighton <bryan.leigh77@gmail.com>
2018-05-31 16:36:24 +0300

    debug for deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  # config.cache_classes = true
+  config.cache_classes = true
   # config.assets.css_compressor = :yui
   # config.assets.js_compressor = :uglifier
 
